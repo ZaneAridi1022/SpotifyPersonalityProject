ea199ff8890f857d987e85d579759008134d11d3
KaijiaZhan <97995975+KaijiaZhan@users.noreply.github.com>
2022-11-13 22:49:49 -0500

    commit

--- SpotifyPersonalityPredictor/FrontEnd/pages/Step4.py
+++ SpotifyPersonalityPredictor/FrontEnd/pages/Step4.py
@@ -4,3 +4,34 @@ import streamlit as st
 
 st.title("Predict your Rythm")
 
+st.write("Thank you for your participation!")
+
+ques1 = st.radio("I see myself as someone who is reserved",(1, 2, 3, 4, 5))
+
+ques2 = st.radio("I see myself as someone who is generally trusting",(1, 2, 3, 4, 5))
+
+ques3 = st.radio("I see myself as someone who tends to be lazy",(1, 2, 3, 4, 5))
+
+ques4 = st.radio("I see myself as someone who is relaxed, handles stress well",(1, 2, 3, 4, 5))
+
+ques5 = st.radio("I see myself as someone who has few artistic interests",(1, 2, 3, 4, 5))
+
+ques6 = st.radio("I see myself as someone who is outgoing, sociable",(1, 2, 3, 4, 5))
+
+ques7 = st.radio("I see myself as someone who tends to find fault with others",(1, 2, 3, 4, 5))
+
+ques8 = st.radio("I see myself as someone who does a thorough job",(1, 2, 3, 4, 5))
+
+ques9 = st.radio("I see myself as someone who gets nervous easily",(1, 2, 3, 4, 5))
+
+ques10 = st.radio("I see myself as someone who has an active imagination",(1, 2, 3, 4, 5))
+
+extraversion = (6-ques1) + ques6
+
+agreeableness = (6-ques7) + ques2
+
+conscientiousness = (6-ques3) + ques8
+
+neuroticism = (6-ques4) + ques9
+
+openness = (6-ques5) + ques10
